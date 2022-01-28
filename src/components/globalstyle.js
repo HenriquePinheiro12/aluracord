f6b7e0dd2ff50c059e3c4cf68d0be77a08fddc09
HenriquePinheiro <pinheirohsp@gmail.com>
2022-01-28 19:01:46 -0300

    StickersBtn actually added on the directories tree

---
+++ src/components/globalstyle.js
@@ -0,0 +1,82 @@
+import AppConfig from '../../config.json'
+import mainBg from '../../public/assets/images/main-bg.jpg'
+
+const getGlobalCssVariables = () => {
+    const {theme} = AppConfig
+    let rootVariables = ``;
+    Object.keys(theme).forEach(group => {
+        Object.keys(theme[group]).forEach(prop => {
+            rootVariables += `--${prop}:${theme[group][prop]};`
+        })
+    })
+    return rootVariables
+}
+
+const GlobalStyle = () => {
+    return (
+        <>
+            <style global jsx>{`
+                @import url(https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic);
+                
+                :root{${getGlobalCssVariables()}}
+
+                *{
+                    margin:0;
+                    padding:0;
+                    box-sizing:border-box;
+                    list-style:none;
+                    font-family:var(--ff) ;
+                }
+
+                html, body{
+                    min-height: 100vh;
+                    display: flex;
+                }
+
+                html, body, #__next{
+                    max-width:100%;
+                    min-height:100vh;
+                    flex:1;
+                }
+                
+                ::-webkit-scrollbar{
+                    width: 5px
+                }
+
+                ::-webkit-scrollbar-track{
+                    background: transparent;
+                    margin: 1rem;
+                }
+
+                ::-webkit-scrollbar-thumb{
+                    background: var(--grey);
+                    border-radius: 2px;
+                }
+
+                main{
+                    min-height:100vh;
+                    background:url(${mainBg.src});
+                    background-size:cover;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    display:flex;
+                    align-items:center;
+                    justify-content:center
+                }
+
+                .glassmorphism{
+                    background: rgba( 255, 255, 255, 0.15 );
+                    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+                    backdrop-filter: blur( 5.5px );
+                    -webkit-backdrop-filter: blur( 5.5px );
+                    border-radius: 10px;
+                    border: 1px solid rgba( 255, 255, 255, 0.18 );
+                }
+                
+            `}
+            </style>
+        </>
+    )
+}
+
+export default GlobalStyle
