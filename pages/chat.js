6a36e8b0e64ad89dcc16fbd11133a3c1200c7182
HenriquePinheiro <pinheirohsp@gmail.com>
2022-01-30 15:20:02 -0300

    consecutive msgs display feature added

--- pages/chat.js
+++ pages/chat.js
@@ -72,8 +72,9 @@ const Chat = (props) => {
                     <Header/>
                     <ChatBox>
                         {
-                            messages.map(val => {
-                                return <Message message={val} username={username} /> 
+                            messages.map((val, i) => {
+                                return <Message message={val} 
+                                consecutive={Boolean(i < messages.length -1 && messages[i + 1].from === val.from)} /> 
                             })
                         }
                     </ChatBox>
@@ -136,19 +137,25 @@ function ChatBox({children}){ //destructure it if necessary
 }
 
 
-function Message({message, username}){
+function Message({message, consecutive}){
     return(
         <>
-            <Box tag="li" key={message.id}>
-                <Box styleSheet={{display: 'flex', alignItems:'center'}}>
-                    <Image src={`https://github.com/${message.from}.png`} styleSheet={{width:'25px', borderRadius:'50%'}}/>
-                    <Text children={message.from} styleSheet={{margin: '0 var(--spc1)', fontWeight: 'var(--fw3)'}} variant="body3"/>
-                    <Text children={message.date} variant="body4"/>  
+            <Box tag="li" styleSheet={{margin: 'var(--spc1)'}} key={message.id}>
+                <Box styleSheet={{display: 'flex', justifyContent:'space-between',alignItems:'center', margin: 'var(--scp1)'}}>
+                    <Box styleSheet={{display: 'flex'}}>
+                        {(!consecutive && 
+                            <>
+                            <Image src={`https://github.com/${message.from}.png`} styleSheet={{width:'25px', borderRadius:'50%'}}/>
+                            <Text children={message.from} styleSheet={{margin: '0 var(--spc1)', fontWeight: 'var(--fw3)'}} variant="body3"/>
+                            </>
+                        )}
+                    </Box>
+                    <Text children={message.date.replace(/-/g, '/')} styleSheet={{float: 'right'}} variant="body4"/>  
                 </Box>
                 <Text children={
                     message.content.startsWith(':sticker:') ?
-                    <Image src={message.content.replace(':sticker:', '').trim()} styleSheet={{maxWidth: '450px', width: '90%'}} /> : message.content 
-                } styleSheet={{margin: 'var(--spc1)', wordWrap: 'break-word'}}>
+                    <Image src={message.content.replace(':sticker:', '').trim()} styleSheet={{maxWidth: '350px', width: '90%'}} /> : message.content 
+                } styleSheet={{margin: 'var(--spc3)', wordWrap: 'break-word'}}>
                 </Text>
             </Box>
         </>
